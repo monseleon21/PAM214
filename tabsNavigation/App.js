19546cb735a8f0dbefffb98d8dea8743b3064efa
María Monserrat Campuzano León <121038307@upq.edu.mx>
2025-11-19 23:44:16 -0600

    tabsnavigation 17

--- tabsNavigation/App.js
+++ tabsNavigation/App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons} from '@expo/vector-icons';
 
-export default function App() {
+import Home from './screens/home';
+import Profile from "./screens/profile";
+import Settings from "./screens/settings";
+
+const Tab = createBottomTabNavigator ();
+
+export default function App()
+{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions = {({route}) => (
+        {
+          headerShown: false,
+          tabBarIcon: ({color, size}) => 
+          {
+            let iconName;
+            if (route.name === 'Home') 
+            {
+              iconName = 'home';
+            } else if (route.name === 'Profile')
+            {
+              iconName = 'person';
+            } else if (route.name === 'Settings')
+            {
+              iconName = 'settings';
+            }
+            return <Ionicons name = {iconName} size = {size} color = {color} />;
+          },
+          tabBarActiveTintColor: '#007BFF',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: 
+          {
+            paddingBottom: 5,
+            height: 60,
+          },
+        })}
+      >
+        <Tab.Screen name = "Home" component = {Home}/>
+        <Tab.Screen name = "Profile" component = {Profile}/>
+        <Tab.Screen name = "Settings" component = {Settings}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
